0c7791e9f767323cceeb6ef439a949cdd21a6fe8
katrin2404 <sachko.ekaterina@gmail.com>
2017-09-28 22:49:58 +0300

    created my info and photo

---
+++ js/GoogleAnalyticsObject.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Andrew on 28.09.2017.
+ */
+(function(w,g){w['GoogleAnalyticsObject']=g;
+    w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
+// Optional TODO: replace with your Google Analytics profile ID.
+ga('create', 'UA-XXXX-Y');
+ga('send', 'pageview');
